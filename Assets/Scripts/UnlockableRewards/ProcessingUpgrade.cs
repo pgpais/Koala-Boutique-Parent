6995689fcd0efd11ca692d1df0a66c2002fbad65
pgpais <pgpaisdev@gmail.com>
2021-07-01 09:29:38 +0100

    file cleanup

--- Assets/Scripts/UnlockableRewards/ProcessingUpgrade.cs
+++ Assets/Scripts/UnlockableRewards/ProcessingUpgrade.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 [CreateAssetMenu(fileName = "ProcessingUpgrade", menuName = "Ye Olde Shop/ProcessingUpgrade", order = 0)]
 public class ProcessingUpgrade : ScriptableObject, UnlockableReward
 {
