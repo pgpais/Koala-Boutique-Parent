d8bd872c639cf6237b0fa15cec0292718de4141f
pgpais <pgpaisdev@gmail.com>
2021-08-18 10:17:40 +0100

    fixed currency layout not updating

--- Assets/Scripts/UI/Shop/ShopUI.cs
+++ Assets/Scripts/UI/Shop/ShopUI.cs
@@ -3,43 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using System.Linq;
 
 public class ShopUI : MonoBehaviour
 {
     [SerializeField] string shopName;
 
     [Header("References")]
+    [SerializeField] RectTransform layout;
     [SerializeField] TMP_Text goldText;
     [SerializeField] TMP_Text gemText;
     [SerializeField] TMP_Text shopPresenceText;
     [SerializeField] TMP_Text shopNameText;
 
-    // public bool IsPresent
-    // {
-    //     get
-    //     {
-    //         return IsPresent;
-    //     }
-    //     set
-    //     {
-    //         IsPresent = value;
-    //         shopPresenceText.enabled = IsPresent;
-    //     }
-    // }
-
     [Header("Debug")]
     [SerializeField] bool isPresent;
 
+    private RectTransform[] layoutGroups;
+
     private void Awake()
     {
+        var children = layout.GetComponentsInChildren<LayoutGroup>();
+        layoutGroups = children.Select(layout => layout.GetComponent<RectTransform>()).ToArray();
+
         GoldManager.GoldChanged.AddListener((gold) =>
         {
+            foreach (var group in layoutGroups)
+            {
+                group.gameObject.SetActive(false);
+                group.gameObject.SetActive(true);
+            }
+
             goldText.text = gold.ToString();
         });
 
         GoldManager.GemChanged.AddListener((gem) =>
         {
+            foreach (var group in layoutGroups)
+            {
+                group.gameObject.SetActive(false);
+                group.gameObject.SetActive(true);
+            }
+
             gemText.text = gem.ToString();
         });
     }
